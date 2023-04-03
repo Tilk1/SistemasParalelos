0238475d9b72ea583adc4303145de5b9e3714f92
Cath Gaitan <catu.gaitan@gmail.com>
2023-04-03 16:42:33 -0300

    esto no vaa acaa

--- TP1/script_mayorMatriz.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#SBATCH -N 1
-#SBATCH --exclusive
-#SBATCH -o ./outputs/outputMayorMatriz.txt
-#SBATCH -e ./outputs/erroresMayorMatriz.txt
-./mayorMatriz 4096 #es codigo secuencial
--- TP1/script_potencia2Matriz.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#SBATCH -N 1
-#SBATCH --exclusive
-#SBATCH -o ./outputs/outputPotencia2Matriz.txt
-#SBATCH -e ./outputs/erroresPotencia2Matriz.txt
-./mayorMatriz 4096 #es codigo secuencial
