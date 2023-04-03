390f3f80fa22e43acfc2445f884635900ad1791b
Cath Gaitan <catu.gaitan@gmail.com>
2023-04-03 16:08:35 -0300

    ahora si anda calcular max

--- TP1/Ecuacion.c
+++ TP1/Ecuacion.c
@@ -14,8 +14,10 @@ double dwalltime(){
 int main(int argc, char *argv[]){
     double *A,*B,*C,*R;
     int *D;
-    double timeBloques,tick,maxA,minA,maxB,minB;
-    int i,j,k,N,tam_bloque=8;
+    double timeBloques,tick,minA,maxB,minB;
+    int i,j,k,l,N,tam_bloque=4;
+    double maxA = -INFINITY;
+    double block_maxA = -INFINITY;
 
     //Verificar parametro 
     if ((argc != 2)){
@@ -23,7 +25,7 @@ int main(int argc, char *argv[]){
         exit(1);
     }
     N = atoi(argv[1]);
-    if((N < 511) || (N > 4097) || (((N % 2)!=0))){
+    if((N < 1) || (N > 4097) || (((N % 2)!=0))){
         printf("El N debe ser: 512,1024,2048,4096\n");
         exit(1);
     }
@@ -37,34 +39,44 @@ int main(int argc, char *argv[]){
     //Inicializar matrices
     for(i=0;i<N;i++){
         for(j=0;j<N;j++){
-            A[i*N+j]=rand() % 999999999 + 1; //Ordenada por FIlas
+            A[i*N+j]=i+j; //Ordenada por Filas
             B[j*N+i]=1; //Ordenada por Columnas
             C[i*N+j]=1; //Ordenada Por Filas
             D[j*N+i]=rand() % 40 + 1; //Ordenada por Columnas
+        }
+    }
+
+    for(i=0;i<N;i++){
+        for(j=0;j<N;j++){
+            printf("Fila: %i, Columna: %i, valor: %f\n",i,j,A[i*N+j]);
         }
     }
     
     //EMPIEZA A CONTAR EL TIEMPO
     tick = dwalltime();
 
-    //calcular Maximo de A
-    for(i=0;i<N;i+= tam_bloque){
-        for(j=0;j<N;j+= tam_bloque){
-            int block_max = A[i*N+j];
-            int k, l;
+    for(i=0; i < N ; i += tam_bloque){
+        for(j=0; j < N ; j += tam_bloque){
+            block_maxA = A[i*N+j];
             for(k = i; k < i + tam_bloque; k++){
                 for(l = j; l < j + tam_bloque; l++){
-                    if(A[k*N+l] > block_max){
-                        block_max = A[k*N+l];
+                    if(A[k*N+l] > block_maxA){
+                        block_maxA = A[k*N+l];
                     }
                 }
             }
-            if(A[i*N+j] > maxA){
-                maxA = A[i*N+j];
+            if(block_maxA > maxA){
+                maxA = block_maxA;
             }
         }
     }
 
     timeBloques = dwalltime() - tick;
-    printf("Tiempo requerido solucion por bloques: %f y el resultado es %f\n",timeBloques,maxA);
+    printf("Tiempo requerido solucion por bloques: %f y el max es: %f\n",timeBloques,maxA);
+
+    free(A);
+    free(B);
+    free(C);
+    free(D);
+    free(R);
 }
