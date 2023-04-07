29baf5677240b4878d30488b89c4f9d51ef000f7
Cath Gaitan <catu.gaitan@gmail.com>
2023-04-07 15:54:57 -0300

    agrego ;

--- TP1/comp_BuscarMax_Min.c
+++ TP1/comp_BuscarMax_Min.c
@@ -71,7 +71,7 @@ int main(int argc, char *argv[]){
     maxA=-INFINITY;
     maxB=-INFINITY;
     minA=+INFINITY; 
-    minB=+INFINITY
+    minB=+INFINITY;
     tick = dwalltime();
     for(i=0;i<N;i++){
         for(j=0;j<N;j++){
