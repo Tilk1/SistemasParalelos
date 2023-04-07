d0d6aee5b0699992323192f5e96e332864074303
Cath Gaitan <catu.gaitan@gmail.com>
2023-04-07 19:09:07 -0300

    mult matrices

--- TP1/mult_matrices.c
+++ TP1/mult_matrices.c
@@ -13,11 +13,10 @@ double dwalltime(){
 }
 
 int main(int argc, char *argv[]){
-    double *A,*B;
-    double time_unFor,time_dosFor,time_Bloque,tick;
+    double *A,*B,*C;
+    double time_ordenado,time_desordenado,tick;
     int i,j,k,l,N;
-    double ;
-    int tam_bloque = 128;
+    int tam_bloque=32;
 
     N = atoi(argv[1]);
     //Verificar parametro 
@@ -29,4 +28,78 @@ int main(int argc, char *argv[]){
     //Aloca memoria para las matrices
     A=(double*)malloc(sizeof(double)*N*N);
     B=(double*)malloc(sizeof(double)*N*N);
+    C=(double*)malloc(sizeof(double)*N*N);
+
+    //Inicializa las matrices
+    for(i=0;i<N;i++){
+        for(j=0;j<N;j++){
+            A[i*N+j]=i+j; //ordenado por fila
+            B[j*N+i]=i*j; //ordenado por columna
+        }
+    }
+
+    tick = dwalltime();
+    //multiplicacion de matrices , matriz A ordenada por filas, B por columna
+    for(i=0;i<N;i++){
+        for(j=0;j<N;j++){
+            double valorC = 0; 
+            for(k=0;k<N;k++){
+                valorC += A[i*N+k]*B[k*N+j]; 
+            }
+            C[i*N+j] = valorC;
+        }
+    }   
+    double time_desordenada_sin_bloq = dwalltime() - tick;
+    printf("Tiempo requerido para calcular time_desordenada_sin_bloq: %f\n",time_desordenada_sin_bloq);
+    
+    tick = dwalltime();
+    //multiplicacion de matrices , matriz B ordenada por filas, A por columna
+    for(i=0;i<N;i++){
+        for(j=0;j<N;j++){
+            double valorC = 0; 
+            for(k=0;k<N;k++){
+                valorC += A[k*N+i]*B[j*N+k];//intercambia indices
+            }
+            C[i*N+j] = valorC; 
+        }
+    }
+    double time_ordenado_sin_bloq = dwalltime() - tick;
+    printf("Tiempo requerido para calcular time_ordenado_sin_bloq: %f\n",time_ordenado_sin_bloq);
+
+
+    //Por bloques
+    tick = dwalltime();
+    for (i = 0; i < N; i += tam_bloque){
+        for (j = 0; j < N; j += tam_bloque){
+            for(k = 0; k < N; k += tam_bloque){
+                multBloque(&A[i*N + k], &B[j*N + k], &C[i*N + j],N,tam_bloque);
+            }
+        }
+    }
+    time_bloque = dwalltime() - tick;
+    printf("Tiempo requerido para calcular por bloque: %f\n",time_bloque);
 }
+
+void multBloque(double *bloqueA, double *bloqueB, double *bloqueC, int N,int tam_bloque){
+    int i, j, k;
+    double valorC=0;
+    for(i=0; i < tam_bloque; i++){
+        for(j = 0; j < tam_bloque; j++){
+            for(k = 0; k < tam_bloque; k++){
+                valorC += bloqueA[i*N+ k] * bloqueB[j*N+k];
+            }
+            bloqueC[i*n+j]=valorC;
+        }
+    }
+}
+
+  //multiplicacion de matrices , matriz A ordenada por filas, B por columna
+    //fila y columnas mal sin bloqe
+    //fila y columna bien sin bloque
+    //fila y columna bien con bloque tam n 32
+    //fila y columna bien con bloque tam n 64
+    //fila y columna bien con bloque tam n 128
+    //fila y columna bien con bloque tam n 256
+    //fila y columna bien con bloque tam optimo CON o1
+    //fila y columna bien con bloque tam optimo CON o2
+    //fila y columna bien con bloque tam optimo CON o3
