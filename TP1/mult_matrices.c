19b1a4bbd01b9231dd6bc7ea232b294dc4573c11
Cath Gaitan <catu.gaitan@gmail.com>
2023-04-07 20:01:42 -0300

    arreglo mult

--- TP1/mult_matrices.c
+++ TP1/mult_matrices.c
@@ -12,9 +12,22 @@ double dwalltime(){
 	return sec;
 }
 
+void multBloque(double *bloqueA, double *bloqueB, double *bloqueC, int N,int tam_bloque){
+    int i, j, k;
+    double valorC=0;
+    for(i=0; i < tam_bloque; i++){
+        for(j = 0; j < tam_bloque; j++){
+            for(k = 0; k < tam_bloque; k++){
+                valorC += bloqueA[i*N+ k] * bloqueB[j*N+k];
+            }
+            bloqueC[i*N+j]=valorC;
+        }
+    }
+}
+
 int main(int argc, char *argv[]){
     double *A,*B,*C;
-    double time_ordenado,time_desordenado,tick;
+    double time_ordenado,time_desordenado,time_bloque,tick;
     int i,j,k,l,N;
     int tam_bloque=32;
 
@@ -78,20 +91,14 @@ int main(int argc, char *argv[]){
     }
     time_bloque = dwalltime() - tick;
     printf("Tiempo requerido para calcular por bloque: %f\n",time_bloque);
+
+    free(A);
+    free(B);
+    free(C);
+    
+    return 0;
 }
 
-void multBloque(double *bloqueA, double *bloqueB, double *bloqueC, int N,int tam_bloque){
-    int i, j, k;
-    double valorC=0;
-    for(i=0; i < tam_bloque; i++){
-        for(j = 0; j < tam_bloque; j++){
-            for(k = 0; k < tam_bloque; k++){
-                valorC += bloqueA[i*N+ k] * bloqueB[j*N+k];
-            }
-            bloqueC[i*n+j]=valorC;
-        }
-    }
-}
 
   //multiplicacion de matrices , matriz A ordenada por filas, B por columna
     //fila y columnas mal sin bloqe
